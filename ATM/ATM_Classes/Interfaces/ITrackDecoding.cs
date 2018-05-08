084077dcafb31584840e5471db4f5ed9566a8c64
Don Bui <marcauphongbui@gmail.com>
2018-05-08 14:10:35 +0200

    Added barebones system by Troels

---
+++ ATM/ATM_Classes/Interfaces/ITrackDecoding.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using ATM_Classes.Data;
+
+namespace ATM_Classes.Interfaces
+{
+    public class TrackDataEventArgs : EventArgs
+    {
+        public List<TrackData> TrackData { get; }
+        public TrackDataEventArgs(List<TrackData> trackData)
+        {
+            TrackData = trackData;
+        }
+    }
+
+    public interface ITrackDecoding
+    {
+        event EventHandler<TrackDataEventArgs> TrackDataReady;
+    }
+}
---
+++ ATM/ATM_Classes/Interfaces/ITrackReceiver.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using ATM_Classes.Data;
+
+namespace ATM_Classes.Interfaces
+{
+    public interface ITrackReceiver
+    {
+        void ReceiveTracks(List<TrackData> tracks);
+    }
+}
